02fe11609e1a6fe2c85018d33d38d5b18aa87cb3
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-07-14 12:29:19 +0530

    Readme Updated

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Contributed Repo Finder
-A Web App To Find And Explore Repositories Where A GitHub User Has Made Contributions.
+
+![Contributed Repo Finder Logo](./icon.webp)
+
+Welcome to Contributed Repo Finder, a React application that allows you to fetch and explore the contributed repositories of a GitHub user. With this app, you can easily discover the repositories and their details with just a few clicks.
+
+## Features
+
+- Enter the GitHub username in the input field and click on "Fetch" to retrieve the contributed repositories.
+- Each repository card displays the name, description, number of stars, and number of forks.
+- Use the pagination buttons to navigate through the repositories. Click the "Previous" button to go to the previous page, and the "Next" button to move to the next page.
+- A sticky "Back to Top" button appears when you scroll down, allowing you to quickly return to the top of the page.
+
+## Getting Started
+
+To run the Contributed Repo Finder locally, follow these steps:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/soumwadeep/contributed-repo-finder.git
+   ```
+
+2. Install the dependencies:
+
+   ```bash
+   cd contributed-repo-finder
+   npm install
+   ```
+
+5. Start the development server:
+
+   ```bash
+   npm start
+   ```
+
+6. Open your browser and navigate to `http://localhost:3000` to access the Contributed Repo Finder.
+
+## Technologies Used
+
+- React
+- Axios
+- CSS
+
+## Screenshots
+
+![Contributed Repo Finder Screenshot](./screenshot.png)
+
+## Acknowledgments
+
+- [GitHub API](https://docs.github.com/en/rest)
+
+---
+
+© 2023. All rights reserved by Contributed Repo Finder.
--- src/App.jsx
+++ src/App.jsx
@@ -63,82 +63,85 @@ const App = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="text-center mb-4">
-        <img src={logo} alt="logo" className="logo"/> Welcome To Contributed Repo Finder
-      </h1>
-      <h4 className="text-center mb-3">
-        Enter The Github Username Here And Click On Fetch To Fetch The
-        Contributed Repos Of The User:
-      </h4>
-      <div className="input-group flex-nowrap">
-        <span className="input-group-text" id="addon-wrapping">
-          @
-        </span>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Username"
-          aria-label="Username"
-          aria-describedby="addon-wrapping"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button
-          className="btn btn-info"
-          type="button"
-          id="button-addon2"
-          onClick={fetchContributedRepositories}
-        >
-          Fetch
-        </button>
-      </div>
-      <div>
-        {repositories.map((repo) => (
-          <div className="card mt-5" key={repo.id}>
-            <div className="card-body">
-              <h3 className="card-title">{repo.name}</h3>
-              <p className="card-text">{repo.description}</p>
-              <button className="btn btn-info me-4">
-                <i className="fa-regular fa-star iconfix"></i>
-                {repo.stargazers_count}
-              </button>
-              <button className="btn btn-info">
-                <i className="fa-solid fa-code-fork iconfix"></i>
-                {repo.stargazers_count}
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-      {repositories.length > 0 && (
-        <div className="mt-4">
+    <>
+      <div className="container">
+        <h1 className="text-center mt-3 mb-4">
+          <img src={logo} alt="logo" className="logo" /> Welcome To Contributed
+          Repo Finder
+        </h1>
+        <h4 className="text-center mb-3">
+          Enter The Github Username Here And Click On Fetch To Fetch The
+          Contributed Repos Of The User:
+        </h4>
+        <div className="input-group flex-nowrap">
+          <span className="input-group-text" id="addon-wrapping">
+            @
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <button
-            onClick={handlePreviousPage}
-            className="btn btn-warning me-3"
-            disabled={page === 1}
+            className="btn btn-info"
+            type="button"
+            id="button-addon2"
+            onClick={fetchContributedRepositories}
           >
-            <i className="fa-solid fa-circle-chevron-left iconfix"></i>
+            Fetch
           </button>
-          {hasNextPage && (
-            <button className="btn btn-warning" onClick={handleNextPage}>
-              <i className="fa-solid fa-circle-chevron-right iconfix"></i>
-            </button>
-          )}
         </div>
-      )}
-      {showBackToTop && (
-        <button
-          className="btn btn-info sticky-bottom btnpos"
-          onClick={handleScrollToTop}
-        >
-          <i className="fa-solid fa-circle-chevron-up iconfix"></i>
-        </button>
-      )}
+        <div>
+          {repositories.map((repo) => (
+            <div className="card mt-5" key={repo.id}>
+              <div className="card-body">
+                <h3 className="card-title">{repo.name}</h3>
+                <p className="card-text">{repo.description}</p>
+                <button className="btn btn-info me-4">
+                  <i className="fa-regular fa-star iconfix"></i>
+                  {repo.stargazers_count}
+                </button>
+                <button className="btn btn-info">
+                  <i className="fa-solid fa-code-fork iconfix"></i>
+                  {repo.stargazers_count}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        {repositories.length > 0 && (
+          <div className="mt-4">
+            <button
+              onClick={handlePreviousPage}
+              className="btn btn-warning me-3"
+              disabled={page === 1}
+            >
+              <i className="fa-solid fa-circle-chevron-left iconfix"></i>
+            </button>
+            {hasNextPage && (
+              <button className="btn btn-warning" onClick={handleNextPage}>
+                <i className="fa-solid fa-circle-chevron-right iconfix"></i>
+              </button>
+            )}
+          </div>
+        )}
+        {showBackToTop && (
+          <button
+            className="btn btn-info sticky-bottom btnpos"
+            onClick={handleScrollToTop}
+          >
+            <i className="fa-solid fa-circle-chevron-up iconfix"></i>
+          </button>
+        )}
+      </div>
       <footer className="text-center mt-3">
         <h5>Copyright 2023. All Rights Reserved By Cotributed Repo Finder.</h5>
       </footer>
-    </div>
+    </>
   );
 };
 
