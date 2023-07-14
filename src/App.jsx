7b89ab77f06eaea33ae0b56f1c68bfddaa581420
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-07-14 10:50:00 +0530

    Show Results More Than 30

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,81 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [repositories, setRepositories] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (username !== "") {
+      fetchContributedRepositories();
+    }
+  }, [page]);
 
   const fetchContributedRepositories = async () => {
     try {
-      const response = await fetch(
-        `https://api.github.com/users/${username}/repos?type=all`
+      const response = await axios.get(
+        `https://api.github.com/users/${username}/repos?type=all&page=${page}&per_page=30`
       );
-      const data = await response.json();
-      setRepositories(data);
+      setRepositories((prevRepositories) => [
+        ...prevRepositories,
+        ...response.data,
+      ]);
     } catch (error) {
       console.error("Error fetching repositories:", error);
     }
   };
 
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
   return (
-    <div>
-      <h1>Github Contributed Repository List</h1>
-      <input
-        type="text"
-        placeholder="Enter a GitHub username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button onClick={fetchContributedRepositories}>
-        Fetch Contributed Repositories
-      </button>
-      <ul>
-        {repositories.map((repo) => (
-          <li key={repo.id}>
-            <h3>{repo.name}</h3>
-            <p>{repo.description}</p>
-            <p>Stars: {repo.stargazers_count}</p>
-            <p>Forks: {repo.forks_count}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section>
+      <div className="container">
+        <h1 className="text-center">Welcome To Contributed Repo Finder</h1>
+        <h4 className="text-center mb-3">
+          Enter The Github Username Here And Click On Fetch To Fetch The
+          Contributed Repos Of The User:
+        </h4>
+        <div className="input-group flex-nowrap">
+          <span className="input-group-text" id="addon-wrapping">
+            @
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button
+            className="btn btn-info"
+            type="button"
+            id="button-addon2"
+            onClick={fetchContributedRepositories}
+          >
+            Fetch
+          </button>
+        </div>
+        <ul>
+          {repositories.map((repo) => (
+            <div key={repo.id}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <p>Stars: {repo.stargazers_count}</p>
+              <p>Forks: {repo.forks_count}</p>
+            </div>
+          ))}
+        </ul>
+        {repositories.length > 0 && (
+          <button onClick={handleLoadMore}>Load More</button>
+        )}
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
